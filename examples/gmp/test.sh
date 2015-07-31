680ca029b9e2bcf8eeab6d8f9f7e254d4e06bd68
Soonho Kong <soonhok@cs.cmu.edu>
2015-07-31 09:18:49 -0700

    remove examples

--- examples/gmp/test.sh
+++
@@ -1,2 +0,0 @@
-emcc test.c -I ../../includes/gmp ../../libs/libgmp.so -o test.js
-nodejs test.js 2000
--- examples/mpfr/test.sh
+++
@@ -1,2 +0,0 @@
-emcc test.c ../../libs/libmpfr.so ../../libs/libgmp.so -I ../../includes/gmp -I ../../includes/mpfr -o test.js
-nodejs test.js
